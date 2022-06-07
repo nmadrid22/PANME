82102c4b7c31f7594a1600def4b5d837662c057b
Dell <Dell@DESKTOP-97HN4GD>
2022-06-07 12:34:01 -0600

    Agregando tabla para base de datos

--- PANME/PANME.BL/Contexto.cs
+++ PANME/PANME.BL/Contexto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,15 @@ namespace PANME.BL
 {
     public class Contexto: DbContext
     {
-        public Contexto(): base("PANMEDB") 
+        public Contexto(): base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\QuieroPizzaDB.mdf")
         {
 
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
         public DbSet<Producto> Productos { get; set; }
--- PANME/PANME.BL/ProductosBL.cs
+++ PANME/PANME.BL/ProductosBL.cs
@@ -12,35 +12,13 @@ namespace PANME.BL
 
         public ProductosBL()
         {
-
             _contexto = new Contexto();
         }
-
+        
         public List<Producto> ObtenerProductos()
         {
-                        
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Bolsa de Pan Molde";
-            producto1.Precio = 65;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Bolsa de Pan Blanco";
-            producto2.Precio = 35;
-
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Dulces de Leche";
-            producto3.Precio = 25;
-
-           var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
-        }
+            return _contexto.Productos.ToList();
+       
+       }
     }
 }
